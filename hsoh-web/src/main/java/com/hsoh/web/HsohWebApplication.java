ea4cb1613c1276694d6c8ad5d08f5e38024b2954
araohs <smlkts@gmail.com>
2015-12-09 21:30:04 +0900

    first commit

---
+++ hsoh-web/src/main/java/com/hsoh/web/HsohWebApplication.java
@@ -0,0 +1,12 @@
+package com.hsoh.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsohWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsohWebApplication.class, args);
+    }
+}
